246b0b419474f5a7992724a65e6964ee6503599e
Nicolas Chagnet <nchagnet@protonmail.com>
2025-02-23 18:43:31 +0100

    Put model options parameters inside their own objects

--- llm_tool_commit/config.py
+++ llm_tool_commit/config.py
@@ -1,21 +1,29 @@
 from pydantic import BaseModel, confloat, conint
 
 
+class ModelOptions(BaseModel):
+    """Class to store options to give the model.
+
+    Attributes:
+        temperature (float): Temperature of the model. Should be between 0.0 and 1.0. Defaults to 0.8.
+        top_p (float): Top p cumulative probability truncation. Should be between 0.0 and 1.0.  Defaults to 0.9.
+        top_k (int): Top k tokens truncation. Should be greater than 1. Defaults to 40.
+    """
+
+    temperature: confloat(ge=0.0, le=1.0) = 0.8
+    top_p: confloat(ge=0.0, le=1.0) = 1.0
+    top_k: conint(ge=1) = 40
+
+
 class ToolConfiguration(BaseModel):
     """Configuration class.
 
     Attributes:
         model (str): Name of model to use (available through Ollama). Defaults to Qwen2.5-coder with 1.5B parameters.
         max_size (int): Maximum size of the git diff to use before truncating. Should be greater than 1. Defaults to 4096.
-        temperature (float): Temperature of the model. Should be between 0.0 and 1.0. Defaults to 0.8.
-        top_p (float): Top p cumulative probability truncation. Should be between 0.0 and 1.0.  Defaults to 0.9.
-        top_k (int): Top k tokens truncation. Should be greater than 1. Defaults to 40.
         message_max_length (int): Maximal length of the commit message. Should be an integer greater than 1. Defaults to 150.
     """
 
     model: str = "qwen2.5-coder:1.5b"
     max_size: conint(ge=1) = 4096
-    temperature: confloat(ge=0.0, le=1.0) = 0.8
-    top_p: confloat(ge=0.0, le=1.0) = 1.0
-    top_k: conint(ge=1) = 40
     message_max_length: conint(ge=1) = 150
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,44 +1,63 @@
 import pytest
 from pydantic import ValidationError
 
-from llm_tool_commit.config import ToolConfiguration
+from llm_tool_commit.config import ModelOptions, ToolConfiguration
+
+
+def test_model_options():
+    """Tests the options model class."""
+    model_options = ModelOptions()
+
+    assert hasattr(model_options, "temperature"), "No attribute `temperature`"
+    assert hasattr(model_options, "top_p"), "No attribute `top_p`"
+    assert hasattr(model_options, "top_k"), "No attribute `top_k`"
+
+    # Check validity of values
+    assert (
+        isinstance(model_options.temperature, float)
+        and model_options.temperature >= 0
+        and model_options.temperature <= 1.0
+    ), "`temperature` should be a floating number between 0.0 and 1.0"
+    assert (
+        isinstance(model_options.top_p, float) and model_options.top_p >= 0.0 and model_options.top_p <= 1.0
+    ), "`top_p` should be a floating number between 0.0 and 1.0"
+    assert (
+        isinstance(model_options.top_k, int) and model_options.top_k >= 1
+    ), "`top_k` should be an integer greater than 1"
 
 
 def test_default_configuration():
-    """Test the initialization of the configuration object."""
+    """Tests the initialization of the configuration object."""
     config = ToolConfiguration()
 
     # Check each attribute
     assert hasattr(config, "model"), "No attribute `model`"
     assert hasattr(config, "max_size"), "No attribute `max_size`"
-    assert hasattr(config, "temperature"), "No attribute `temperature`"
-    assert hasattr(config, "top_p"), "No attribute `top_p`"
-    assert hasattr(config, "top_k"), "No attribute `top_k`"
+    assert hasattr(config, "model_options"), "No attribute `model_options`"
     assert hasattr(config, "message_max_length"), "No attribute `message_max_length`"
 
     # Check validity of values
     assert isinstance(config.model, str), "`model` should a string"
     assert isinstance(config.max_size, int) and config.max_size >= 1, "`max_size` should be an integer greater than 1"
     assert (
-        isinstance(config.temperature, float) and config.temperature >= 0 and config.temperature <= 1.0
-    ), "`temperature` should be a floating number between 0.0 and 1.0"
-    assert (
-        isinstance(config.top_p, float) and config.top_p >= 0.0 and config.top_p <= 1.0
-    ), "`top_p` should be a floating number between 0.0 and 1.0"
-    assert isinstance(config.top_k, int) and config.top_k >= 1, "`top_k` should be an integer greater than 1"
-    assert (
         isinstance(config.message_max_length, int) and config.message_max_length >= 1
     ), "`message_max_length` should be an integer greater than 1"
 
 
 def test_wrong_configuration():
-    """Test configuration objects with erroneous input."""
+    """Tests configuration objects with erroneous input."""
     with pytest.raises(ValidationError):
         ToolConfiguration(model=0)
 
     with pytest.raises(ValidationError):
         ToolConfiguration(max_size=1.5)
 
+    with pytest.raises(ValidationError):
+        ToolConfiguration(message_max_length=0)
+
+
+def test_wrong_model_options():
+    """Tests model options objects with erroneous input."""
     with pytest.raises(ValidationError):
         ToolConfiguration(temperature=2.0)
 
@@ -53,6 +72,3 @@ def test_wrong_configuration():
 
     with pytest.raises(ValidationError):
         ToolConfiguration(top_k=0)
-
-    with pytest.raises(ValidationError):
-        ToolConfiguration(message_max_length=0)
