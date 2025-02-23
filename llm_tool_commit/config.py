2cd455ae1fa4eb19eb800270aed9e9486c2a1efe
Nicolas Chagnet <nchagnet@protonmail.com>
2025-02-23 16:59:50 +0100

    Added initial tests for configuration; Rewrote configuration with pydantic instead of dataclasses

--- llm_tool_commit/config.py
+++ llm_tool_commit/config.py
@@ -1,20 +1,19 @@
-from dataclasses import dataclass
+from pydantic import BaseModel, confloat, conint
 
 
-@dataclass(slots=True)
-class ToolConfiguration:
+class ToolConfiguration(BaseModel):
     """Configuration class.
 
     Attributes:
         model (str): Name of model to use (available through Ollama). Defaults to Qwen2.5-coder with 1.5B parameters.
-        max_size (int): Maximum size of the git diff to use before truncating. Defaults to 4096.
-        temperature (float): Temperature of the model. Defaults to 0.5.
-        top_p (float): Top p cumulative probability truncation. Defaults to 0.9.
-        top_k (int): Top k tokens truncation. Defaults to 40.
+        max_size (int): Maximum size of the git diff to use before truncating. Should be greater than 1. Defaults to 4096.
+        temperature (float): Temperature of the model. Should be between 0.0 and 1.0. Defaults to 0.8.
+        top_p (float): Top p cumulative probability truncation. Should be between 0.0 and 1.0.  Defaults to 0.9.
+        top_k (int): Top k tokens truncation. Should be greater than 1. Defaults to 40.
     """
 
     model: str = "qwen2.5-coder:1.5b"
-    max_size: int = 4096
-    temperature: float = 0.5
-    top_p: float = 1.0
-    top_k: int = 40
+    max_size: conint(ge=1) = 4096
+    temperature: confloat(ge=0.0, le=1.0) = 0.8
+    top_p: confloat(ge=0.0, le=1.0) = 1.0
+    top_k: conint(ge=1) = 40
