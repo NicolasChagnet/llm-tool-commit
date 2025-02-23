58531b9426e21ad0c23f74f84eb139c7f9b86f2a
Nicolas Chagnet <nchagnet@protonmail.com>
2025-02-23 17:10:59 +0100

    Switched CLI setup from Typer to Click for pydantic support

--- llm_tool_commit/__main__.py
+++ llm_tool_commit/__main__.py
@@ -1,6 +1,4 @@
-import typer
-
 from llm_tool_commit.cli import entrypoint
 
 if __name__ == "__main__":
-    typer.run(entrypoint)
+    entrypoint()
--- llm_tool_commit/cli.py
+++ llm_tool_commit/cli.py
@@ -1,3 +1,11 @@
-def entrypoint():
+import click
+from pydanclick import from_pydantic
+
+from llm_tool_commit.config import ToolConfiguration
+
+
+@click.command()
+@from_pydantic(ToolConfiguration)
+def entrypoint(config: ToolConfiguration):
     """Entry point of the CLI application."""
     pass
